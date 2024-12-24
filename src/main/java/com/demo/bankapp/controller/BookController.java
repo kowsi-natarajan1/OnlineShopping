eb33aa011fc69fda1a4c598caf6ca7b279b1616e
kowsinatarajan <kowsi.natarajan0@gmail.com>
2024-12-24 07:51:53 +0530

    initial repo setup commit

---
+++ src/main/java/com/demo/bankapp/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.demo.bankapp.controller;
+
+import com.demo.bankapp.model.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+@RequestMapping("/books")
+public interface BookController {
+
+    @GetMapping("/getAllBooks")
+    public List<Book> getAllBooks();
+}
